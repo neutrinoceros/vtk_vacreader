936f0c891a55d2c6a5be53d9590c02439ba906d6
Clément Robert <clement.robert@oca.eu>
2018-05-23 15:25:05 +0200

    add docstrings

--- vtk_vacreader/__init__.py
+++ vtk_vacreader/__init__.py
@@ -1,4 +1,4 @@
-'''A simple data holder class for non amr runs.'''
+'''This is a minimal wrapper for automating the vtk -> numpy conversion.'''
 
 import numpy as np
 import vtk
@@ -6,8 +6,12 @@ from vtk.util import numpy_support as nps
 
 
 class VacDataSorter:
+    '''A simple data holder class for non-amr runs.'''
+
     _readers = {
-        'vtu': vtk.vtkXMLUnstructuredGridReader #note : StructuredGrid can not be used because dimensions are not stored by AMRVAC (as they usually don't make sense)
+        #note : StructuredGrid can not be used because dimensions
+        #are not stored by AMRVAC (as they usually don't make sense)
+        'vtu': vtk.vtkXMLUnstructuredGridReader
     }
 
     def __init__(self, file_name:str, data_shape:tuple=None):
@@ -32,9 +36,27 @@ class VacDataSorter:
                 self.fields[k].shape = data_shape
 
     def __getitem__(self, key):
+        '''Mimic the behavior of the embedded dictionnary for scrapping/iteration.'''
         return self.fields[key]
 
+    def get_axis(self, axis:int=0):
+        '''Reconstruct an evenly spaced array for cell coordinates along given axis.'''
+        axis_bounds = self.reader.GetOutput().GetBounds()[2*axis : 2*(axis+1)]
+        return np.linspace(*axis_bounds, self.data_shape[axis])
+
+    def get_meshgrid(self, dim:int=2):
+        '''Reconstruct an evenly spaced (2d) grid tuple to locate cells.
+
+        This is meant to be used as input for matplotlib.pyplot.pcolormesh()
+        '''
+        if dim != 2:
+            raise NotImplementedError
+        vectors = [self.get_axis(ax) for ax in range(dim)]
+        vectors.reverse()
+        return np.meshgrid(*vectors)
+
     def _get_sort_key(self):
+        '''Allow reordering of data points against location in the physical space.'''
         data = self.reader.GetOutput()
         raw_cell_coords = np.empty((data.GetNumberOfCells(), 3))
         for i in range(data.GetNumberOfCells()):
@@ -46,14 +68,3 @@ class VacDataSorter:
             dtype=[('r', 'f4'), ('phi', 'f4'), ('z', 'f4')]
         )
         return cell_coords.argsort(order=['r', 'phi'])
-
-    def get_axis(self, axis:int=0):
-        axis_bounds = self.reader.GetOutput().GetBounds()[2*axis : 2*(axis+1)]
-        return np.linspace(*axis_bounds, self.data_shape[axis])
-
-    def get_meshgrid(self, dim:int=2):
-        if dim != 2:
-            raise NotImplementedError
-        vectors = [self.get_axis(ax) for ax in range(dim)]
-        vectors.reverse()
-        return np.meshgrid(*vectors)
